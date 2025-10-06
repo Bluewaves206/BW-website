56758e090d4c2b2429a9c21310d34ed195d2b0e7
Bluewaves206 <developerbluewaves206@gmail.com>
2025-10-06 19:03:55 +0530

    about page content done but responisve is left

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Layout from "./layout/layout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import About from "./pages/about";
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route element={<Layout />} >
             <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<div>About</div>} />
+            <Route path="/about" element={<About/>} />
             <Route path="/services" element={<div>Servies</div>} />
             <Route path="/blog" element={<div>Blog</div>} />
             <Route path="/pricing" element={<div>Pricing</div>} />
